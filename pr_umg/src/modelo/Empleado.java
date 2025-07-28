6f27f2ba35717d11e8e12565252badced467f28a
Eddy-Soto <esotoh@miumg.edu.gt>
2025-07-27 23:45:27 -0600

    Add files via upload

---
+++ pr_umg/src/modelo/Empleado.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Programador
+ */
+public class Empleado extends Persona {
+    private String codigo;
+
+    public Empleado() {
+    }
+
+    public Empleado(String codigo, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+    
+    
+    
+}
